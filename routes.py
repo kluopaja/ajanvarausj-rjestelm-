431e9d824504132f0ef061a0005fd5178436655f
kluopaja <kalle.luopajarvi@helsinki.fi>
2020-07-31 20:19:20 +0300

    Impelement routes for login, logout and registration

--- routes.py
+++ routes.py
@@ -13,3 +13,67 @@ def index():
     return render_template("index.html", polls=polls)
 
 
+#storing the 'login_redirect' in the session was a very bad idea
+#what if the user visits the link, then does something else, 
+#comes back to the site and logs in
+#then they will be redirected to the link site
+
+#how to login and then return to the same page?
+
+
+#TODO give the message as a GET parameter?
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    def redirect_to_next(default='/'):
+        if 'login_redirect' not in session:
+            return redirect(default)
+
+        #if the redirect target is not a valid relative url
+        url = session['login_redirect']
+        del session['login_redirect']
+
+        if not url or url_parse(url).netloc != '':
+            return redirect(default)
+
+        return redirect(url)
+
+    if session.get('user_id', 0):
+        return redirect_to_next(default='/');
+
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    elif request.method == 'POST':
+        print("post request", request.form)
+
+        if process_login(request.form['username'], request.form['password']):
+            return redirect_to_next(default='/')
+
+    return render_template('login.html', 
+                           message='Kirjautuminen epäonnistui,yritä uudelleen')
+
+
+@app.route('/logout')
+def logout():
+    process_logout()
+    
+    return render_template("logout.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    if request.method == 'POST':
+        print("register: ", request.form)
+        is_ok = process_registration(request.form['username'],
+                                      request.form['password'])
+        #after successful registration, automatically log the user in
+        #and redirect to login
+        if is_ok:
+            process_login(request.form['username'],
+                          request.form['password'])
+            return redirect('/login') 
+
+    return render_template("register.html",
+                           message='Käyttäjätunnuksen luonti epäonnistui,yritä uudelleen')
+
