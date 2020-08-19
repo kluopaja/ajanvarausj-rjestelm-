9dd393fa73ffa611d0da1c5e06f90d5a74953d18
kluopaja <kalle.luopajarvi@helsinki.fi>
2020-08-20 00:25:31 +0300

    Modify CSP to work on firefox

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_talisman import Talisman
 from flask_login import LoginManager
 from os import getenv
-
+csp = {
+    'default-src': '\'self\'',
+    'script-src': '\'self\'',
+}
 app = Flask(__name__)
-Talisman(app, content_security_policy_nonce_in=['default-src'])
+Talisman(app, content_security_policy=csp,content_security_policy_nonce_in=['script-src'])
 
 app.secret_key = getenv("SECRET_KEY")
 
