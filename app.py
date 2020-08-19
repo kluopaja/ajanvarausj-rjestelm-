c68bf4f70fa37a6edfb429c4c6702f51a4113820
kluopaja <kalle.luopajarvi@helsinki.fi>
2020-08-19 16:41:38 +0300

    Force https with flask-talisman

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_talisman import Talisman
 from flask_login import LoginManager
 from os import getenv
+
+
+
 app = Flask(__name__)
+Talisman(app)
 app.secret_key = getenv("SECRET_KEY")
 
 import routes
