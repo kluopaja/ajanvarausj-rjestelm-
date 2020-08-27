133fd328d823600ff3a04d05e5f9007f813c1ab0
kluopaja <kalle.luopajarvi@helsinki.fi>
2020-08-27 23:23:13 +0300

    Add 'copy link' buttons to owner page
        -Add poll_owner.js to implement the copying to clipboard
        -Place the url into a <input type="text"> to enable the copying
         to clipboard and to limit the horizontal space usage
        -Reorder some columns in the <table> elements to place the 'copy link'
         button next to the url

---
+++ static/poll_owner.js
@@ -0,0 +1,16 @@
+copyToClipboard = function(e) {
+    let url = document.getElementById(e.target.id)
+    url.select()
+    document.execCommand("copy")
+    alert("Kopioitu leikepöydälle: " + url.value)
+}
+window.addEventListener('load', (e) => {
+    let buttons = document.querySelectorAll(".copy_button")
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', copyToClipboard);
+    }
+    console.log(buttons);
+});
+
+
+
